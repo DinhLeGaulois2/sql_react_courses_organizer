22c8bb8d391a6bb262a5fdfec0a4d6b78376f72e
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-01 05:00:46 -0400

    Backend: Done. Need Tests

--- server/routes/api-routes-read.js
+++ server/routes/api-routes-read.js
@@ -30,18 +30,26 @@ module.exports = function (app) {
     })
 
     app.get("/api/get/instructor/:id", (req, res) => {
-
+        db.person.findOne({ where: { id: req.body.id } })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json(err))
     })
 
     app.get("/api/get/instructor/all", (req, res) => {
-
+        db.person.findAll({ where: { type: 'instructor' } })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json(err))
     })
 
     app.get("/api/get/student/:id", (req, res) => {
-
+        db.person.findOne({ where: { id: req.body.id } })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json(err))
     })
 
     app.get("/api/get/student/all", (req, res) => {
-
+        db.person.findAll({ where: { type: 'student' } })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json(err))
     })
 }
