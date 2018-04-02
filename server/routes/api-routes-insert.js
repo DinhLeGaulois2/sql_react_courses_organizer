3ba790733b7dfe269d0a652de67a0920d9b6c6af
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-02 04:22:56 -0400

    Using 'Transaction' with Sequelize

--- server/routes/api-routes-insert.js
+++ server/routes/api-routes-insert.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const models = require('../models')
+var sequelize = models.sequelize
 
 const Op = Sequelize.Op;
 
@@ -24,7 +26,7 @@ module.exports = function (app) {
             where: {
                 title: dpt.title,
                 credits: dpt.credits,
-                dptId: dpt.dptId
+                departmentId: dpt.dptId
             }
         }).then(data => res.status(200).json(data))
             .catch(err => res.status(400).json(err))
@@ -60,41 +62,44 @@ module.exports = function (app) {
 
     app.post("/api/add/course-instructor", (req, res) => {
         const dpt = req.body
-        db.person.findOne({ where: { id: dpt.instructorId }
-        }).then(data => {
-            if(data==null) res.status(400).json("Could not find an instructor with an ID of '" + dpt.instructorId + "'")
-            else{
-                db.course.findOne({ where: { id: dpt.courseId }})
-                .then(data => {
-                    db.courseIntructor.findOrCreate({ where: {
-                        courseId: dpt.courseId,
-                        personId: dpt.instructorId
-                    }}).then(data => res.status(200).json("Insertion Successfully!"))
-                    .catch(err => res.status(400).json("Could not insert course-instructor, err: " + err))
-                })
-                .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
-            }
-            res.status(200).json(data)
-        })
-            .catch(err => res.status(400).json(err))
+
+        return sequelize.transaction(t => {
+            return db.person.findOne({
+                where: { id: dpt.instructorId }
+            }, { transaction: t }).then(data => {
+                return db.course.findOne({ where: { id: dpt.courseId } }, { transaction: t })
+                    .then(data => {
+                        return db.courseInstructor.findOrCreate({
+                            where: {
+                                courseId: dpt.courseId,
+                                personId: dpt.instructorId
+                            }
+                        }) // No "{ transaction: t }" for the last action
+                    })
+            })
+        }).then(data => { res.status(200).json(data) })
+            .catch(err => res.status(400).json("Insertion Err: " + err))
     })
 
     app.post("/api/add/course-student", (req, res) => {
         const dpt = req.body
-        db.person.findOne({ where: { id: dpt.studentId }
+        db.person.findOne({
+            where: { id: dpt.studentId }
         }).then(data => {
-            if(data==null) res.status(400).json("Could not find a student with an ID of '" + dpt.studentId + "'")
-            else{
-                db.course.findOne({ where: { id: dpt.courseId }})
-                .then(data => {
-                    db.courseIntructor.findOrCreate({ where: {
-                        courseId: dpt.courseId,
-                        personId: dpt.studentId,
-                        grade: ""
-                    }}).then(data => res.status(200).json("Insertion Successfully!"))
-                    .catch(err => res.status(400).json("Could not insert course-student, err: " + err))
-                })
-                .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
+            if (data == null) res.status(400).json("Could not find a student with an ID of '" + dpt.studentId + "'")
+            else {
+                db.course.findOne({ where: { id: dpt.courseId } })
+                    .then(data => {
+                        db.courseIntructor.findOrCreate({
+                            where: {
+                                courseId: dpt.courseId,
+                                personId: dpt.studentId,
+                                grade: ""
+                            }
+                        }).then(data => res.status(200).json("Insertion Successfully!"))
+                            .catch(err => res.status(400).json("Could not insert course-student, err: " + err))
+                    })
+                    .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
             }
             res.status(200).json(data)
         })
@@ -102,28 +107,32 @@ module.exports = function (app) {
     })
 
     app.post("/api/set/course/online", (req, res) => {
-        db.course.findOne({ where: { id: req.body.courseId }})
-        .then(data => {
-            db.onlineCourse.findOrCreate({ where: {
-                courseId: req.body.courseId,
-                url: req.body.url
-            }}).then(data => res.status(200).json("Set Course as 'Online' Successfully!"))
-            .catch(err => res.status(400).json("Could not Set Course as 'Online', err: " + err))
-        })
-        .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
+        db.course.findOne({ where: { id: req.body.courseId } })
+            .then(data => {
+                db.onlineCourse.findOrCreate({
+                    where: {
+                        courseId: req.body.courseId,
+                        url: req.body.url
+                    }
+                }).then(data => res.status(200).json("Set Course as 'Online' Successfully!"))
+                    .catch(err => res.status(400).json("Could not Set Course as 'Online', err: " + err))
+            })
+            .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
     })
 
     app.post("/api/set/course/onsite", (req, res) => {
-        db.course.findOne({ where: { id: req.body.courseId }})
-        .then(data => {
-            db.onlineCourse.findOrCreate({ where: {
-                courseId: req.body.courseId,
-                location: req.body.location,
-                day: req.body.day,
-                time: req.body.time
-            }}).then(data => res.status(200).json("Set Course as 'Onsite' Successfully!"))
-            .catch(err => res.status(400).json("Could not Set Course as 'Onsite', err: " + err))
-        })
-        .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
+        db.course.findOne({ where: { id: req.body.courseId } })
+            .then(data => {
+                db.onlineCourse.findOrCreate({
+                    where: {
+                        courseId: req.body.courseId,
+                        location: req.body.location,
+                        day: req.body.day,
+                        time: req.body.time
+                    }
+                }).then(data => res.status(200).json("Set Course as 'Onsite' Successfully!"))
+                    .catch(err => res.status(400).json("Could not Set Course as 'Onsite', err: " + err))
+            })
+            .catch(err => res.status(400).json("Could not find a course with an ID of '" + dpt.courseId + "'"))
     })
 }
--- src/actions/testAction.js
+++ src/actions/testAction.js
@@ -5,24 +5,76 @@ import cst from '../constants/testConstant'
 const testAction = {
     testAPIRequest: () => {
         return (dispatch) => {
-            const dpt = {
-                name: "depart_1",
-                budget: 1000000,
-                startDate: "2018-03-30 04:05:09",
-                administrator: "James Bond"
+            // const obj = {   // new Instructor
+            //     lastName: "Bond",
+            //     firstName: "James",
+            //     hireDate: "04/02/2018",
+            //     enrollmentDate: "04/02/2018",
+            //     type: 'instructor'
+
+            // }
+            // axios.post("/api/add/course", obj)
+            //     .then(data => {
+            //         console.log("Test Request!")
+            //         console.log("Result: " + JSON.stringify(data, null, 5))
+            //         //         dispatch({
+            //         //             type: cst.TEST_RESULT,
+            //         //             payload: data
+            //     })
+            //     .catch(err => console.log("Request error: " + err))
+
+            // const obj = { // new Department
+            //     name: "depart_1",
+            //     budget: 1000000,
+            //     startDate: "2018-03-30 04:05:09",
+            //     administrator: "James Bond"
+            // }
+
+            // axios.post("/api/add/department", obj)
+            //     .then(data => {
+            //         console.log("Test Request!")
+            //         console.log("Result: " + JSON.stringify(data, null, 5))
+            //         //         dispatch({
+            //         //             type: cst.TEST_RESULT,
+            //         //             payload: data
+            //     })
+            //     // })
+            //     .catch(err => console.log("Request error: " + err))
+
+            // const obj = { // new course
+            //     courseId: "course_1",
+            //     title: "First Course",
+            //     credits: "3",
+            //     dptId: 1
+            // }
+
+            // axios.post("/api/add/course", obj)
+            //     .then(data => {
+            //         console.log("Test Request!")
+            //         console.log("Result: " + JSON.stringify(data, null, 5))
+            //         //         dispatch({
+            //         //             type: cst.TEST_RESULT,
+            //         //             payload: data
+            //     })
+            //     // })
+            //     .catch(err => console.log("Request error: " + err))
+
+            const obj = { // new instructor-course
+                instructorId: 1,
+                courseId: 1
             }
 
-            axios.post("/api/add/department", dpt)
+            axios.post("/api/add/course-instructor", obj)
                 .then(data => {
                     console.log("Test Request!")
                     console.log("Result: " + JSON.stringify(data, null, 5))
-                    dispatch({
-                        type: cst.TEST_RESULT,
-                        payload: data
-                    })
+                    //         dispatch({
+                    //             type: cst.TEST_RESULT,
+                    //             payload: data
                 })
+                // })
                 .catch(err => console.log("Request error: " + err))
-        }        
+        }
     }
 }
 
