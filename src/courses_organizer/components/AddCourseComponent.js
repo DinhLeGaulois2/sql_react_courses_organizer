d1080276f25a7c0aeddbcf766774eab0f8a7de7e
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-28 05:18:46 -0400

    Components

--- src/courses_organizer/components/AddCourseComponent.js
+++ src/courses_organizer/components/AddCourseComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class KKKKKKKKK extends React.Component {
+export default class AddCourseComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
